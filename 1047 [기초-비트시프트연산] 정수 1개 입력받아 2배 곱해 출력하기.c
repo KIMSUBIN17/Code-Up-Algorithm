b3c57af2cf05e4708e5033f0d19b7e88679c28e8
KIMSUBIN17 <tnqls24860@naver.com>
2020-08-13 01:53:18 +0900

    Answer : 1029 ~ 1071

---
+++ 1047 [기초-비트시프트연산] 정수 1개 입력받아 2배 곱해 출력하기.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	scanf("%d", &a);
+	printf("%d\n", a << 1); //10을 2배 한 값인 20 이 출력된다.
+
+	return 0;
+}
---
+++ 1048 [기초-비트시프트연산] 한 번에 2의 거듭제곱 배로 출력하기.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d", a << b); //210 = 1024 가 출력된다.
+
+	return 0;
+}
