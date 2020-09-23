08b107853ea9d7ea0a012d20fae33c20bcefd3ff
KIMSUBIN17 <tnqls24860@naver.com>
2020-09-23 12:02:23 +0900

    Answer : 1061 ~ 1070

---
+++ Python/1063_15165555(AC).py
@@ -0,0 +1,7 @@
+﻿a,b=input().split()
+x=int(a)
+y=int(b)
+print(x if x>y else y)
+# python 삼항 연산자는 ? 대신 (a if 조건 else b) 사용
+
+
---
+++ Python/1064_15165632(AC).py
@@ -0,0 +1,5 @@
+﻿a = input().split()
+b = map(int, a)
+print(min(b))
+
+#map 사용하면 리스트의 요소를 지정된 함수로 변경
