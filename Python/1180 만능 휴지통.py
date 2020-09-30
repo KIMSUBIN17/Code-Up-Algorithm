54ed4daddb09823eca949f4d613413610fc15b84
KIMSUBIN17 <tnqls24860@naver.com>
2020-10-01 01:14:12 +0900

    Answer : 1180

---
+++ Python/1180 만능 휴지통.py
@@ -0,0 +1,14 @@
+﻿n = input()
+
+amount = n[1] + n[0]
+amount = int(amount)*2
+
+if(amount>99):
+    amount = amount % 100
+
+if(amount<=50):
+    print(amount)
+    print('GOOD')
+else:
+    print(amount)
+    print('OH MY GOD')
