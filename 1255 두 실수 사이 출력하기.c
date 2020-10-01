eb1730d793bb91005c73dad7360174b232106a0e
KIMSUBIN17 <tnqls24860@naver.com>
2020-10-02 01:41:56 +0900

    Answer : 1255,1277

---
+++ 1255 두 실수 사이 출력하기.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    double num1, num2;
+    double i;
+
+    scanf("%lf %lf", &num1, &num2);
+
+    for (i = num1; i <= num2; i += 0.01)
+       printf("%.2lf ", i);
+
+    return 0;
+}
---
+++ 1277 몇 번째 데이터 출력하기.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int i=0;
+	int n, a;
+	scanf("%d", &n);
+	for(i=1;i<=n; i++ )
+	{
+		scanf("%d",&a);
+		if(i==1) printf("%d ",a);
+		if(i==n/2+1) printf("%d ",a);
+		if(i==n) printf("%d",a);
+				
+	}
+	return 0;
+}
