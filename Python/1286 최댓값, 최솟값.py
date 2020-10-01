ab49e13bdd53f092e3c4577b4b3e152573cb9d8b
KIMSUBIN17 <tnqls24860@naver.com>
2020-10-02 01:45:11 +0900

    Answer : 1286 ~ 1287

---
+++ Python/1286 최댓값, 최솟값.py
@@ -0,0 +1,11 @@
+﻿numbers = []
+
+for i in range(0, 5):
+    number = input()
+    numbers.append(int(number))
+
+numbers.sort()
+    
+print(numbers[len(numbers) - 1])   #최댓값
+print(numbers[0])                   #최솟값
+
