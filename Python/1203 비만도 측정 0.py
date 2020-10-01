2ca3978fca8351afa67f16a268f7d2d28e213069
KIMSUBIN17 <tnqls24860@naver.com>
2020-10-01 17:19:46 +0900

    Answer : 1201 ~ 1207

---
+++ Python/1203 비만도 측정 0.py
@@ -0,0 +1,10 @@
+﻿bmi = input()
+bmi = int(bmi)
+
+if(bmi <= 10):
+    print('정상')
+elif(bmi <= 20):
+    print('과체중')
+else:
+    print('비만')
+
---
+++ Python/1207 윷놀이.py
@@ -0,0 +1,15 @@
+﻿a,b,c,d = map(int,input().split())
+count = a+b+c+d
+# 0: 뒤집어지지 않은 상태, 1:뒤집어진 상태
+# 뒤집어진 개수에 따라 도,개,걸,윷,모 판단
+
+if (count == 1 ):
+    print('도')
+elif (count == 2 ):
+    print('개')
+elif (count == 3 ):
+    print('걸')
+elif (count == 4 ):
+    print('윷')
+else:
+    print('모')
