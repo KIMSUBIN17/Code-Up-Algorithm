6814d4d4747ae4d5d2e8aada4fc45aa64af460fb
KIMSUBIN17 <tnqls24860@naver.com>
2020-10-02 01:42:43 +0900

    Answer : 1251 ~ 1260

---
+++ Python/1254 알파벳 출력하기.py
@@ -0,0 +1,6 @@
+﻿start, end = input().split()
+start = ord(start)
+end = ord(end)
+
+for i in range(start, end+1):
+    print(chr(i), end=' ')
---
+++ Python/1257 두 수 사이의 홀수 출력하기.py
@@ -0,0 +1,7 @@
+﻿startnum,lastnum = input().split()
+startnum = int(startnum)
+lastnum = int(lastnum)
+
+for i in range(startnum, lastnum + 1):
+    if(i % 2 == 1):
+        print(i, end=' ')
