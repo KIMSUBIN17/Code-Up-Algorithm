aa5b0b7f1a10e9848b352ba57928ae95f6b2f29e
KIMSUBIN17 <tnqls24860@naver.com>
2020-10-06 01:02:15 +0900

    Answer : 1295

---
+++ Python/1295 알파벳 대소문자 변환_2.py
@@ -0,0 +1,13 @@
+﻿text = input()
+
+for i in text:
+    if i.isupper():         #isupper():대문자인지 확인
+        print(i.lower(),end='')
+    elif i.islower():       #islower():소문자인지 확인
+        print(i.upper(),end='')
+    else:
+        print(i,end='')
+        
+
+
+
