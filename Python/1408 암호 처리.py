a05b92cd4906efafba439d1aa4dbf83bd179a1c6
KIMSUBIN17 <tnqls24860@naver.com>
2020-10-06 01:21:03 +0900

    Create 1733.c, 1408.py, 1410.py

---
+++ Python/1408 암호 처리.py
@@ -0,0 +1,22 @@
+﻿password = input()
+encrypt1 = ''
+encrypt2 = ''
+
+for i in range(len(password)):
+    asc = ord(password[i]) + 2
+    encrypt1 += chr(asc)
+    asc = int((ord(password[i]) * 7) % 80 + 48)
+    encrypt2 += chr(asc)
+
+print(encrypt1)
+print(encrypt2)
+
+'''
+chr() : 아스키코드값 입력받아 해당하는 문자 출력
+chr(97) => 'a'
+ord() : 문자의 아스키코드 값을 돌려줌
+ord('a') => 97
+ord함수와 chr함수는 반대
+'''
+
+
---
+++ Python/1410 올바른 괄호 1 (괄호 개수 세기).py
@@ -0,0 +1,11 @@
+﻿str = input()
+openBracket = 0
+closeBracket = 0
+
+for i in range(len(str)):
+    if str[i] == '(':
+        openBracket += 1 
+    elif str[i] == ')':
+        closeBracket += 1 
+        
+print(openBracket,closeBracket)
