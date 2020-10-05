eee45f60ef446b3b89d75c2a677749a2c58bd3d0
KIMSUBIN17 <tnqls24860@naver.com>
2020-10-06 01:03:07 +0900

    Answer : 1418, 1419

---
+++ Python/1418 t를 찾아라.py
@@ -0,0 +1,6 @@
+﻿str = input()
+
+for i in range(0, len(str)):  #str길이만큼 반복
+    if str[i] == 't':           #반복중에 str중 t가 들어간 곳이 있으면
+        print(i+1,end=' ')      
+        #줄바꿈없애기 위해 end=''추가, 0부터 시작하기 때문에 위치출력위해 +1
---
+++ Python/1419 love 2.py
@@ -0,0 +1,4 @@
+﻿str = input()
+search = 'love'  #찾을 문자열
+answer = str.count(search)      #문자열 빈도수 찾기 count
+print(answer)
