cedbd187df7e8c997670286f07a5a8cc28b0e9a2
KIMSUBIN17 <tnqls24860@naver.com>
2020-10-08 21:37:03 +0900

    Create 1405.py

---
+++ Python/1405 숫자 로테이션.py
@@ -0,0 +1,16 @@
+﻿#첫째 줄에 숫자의 개수 n이 입력된다.( 1 <= n <= 1,000)
+#둘째 줄에 n개의 정수 k가 공백으로 구분되어 입력된다.(1 <= k <= 1,000)
+#왼쪽으로 하나씩 출력
+
+a = int(input())
+b = list(map(int, input().split()))
+ 
+for i in range(a) : 
+    for j in range(a) :
+        print(b[j], end = ' ')
+    print()
+    for z in range(a-1) :
+        temp = b[z]
+        b[z] = b[z+1]
+        b[z+1] = temp
+
---
+++ Python/1405 숫자 로테이션_2.py
@@ -0,0 +1,12 @@
+﻿import collections
+
+n = int(input())
+numbers = list(map(int, input().split()))
+numbers = collections.deque(numbers)
+numbers.rotate(1)
+
+for i in range(0, n):
+    numbers.rotate(-1)
+    for number in numbers:
+        print(number, end=' ')
+    print()
