c3fac9d2f6bcc396088d9989af4e22ebad48501d
KIMSUBIN17 <tnqls24860@naver.com>
2020-10-08 21:37:29 +0900

    Create 1411.py

---
+++ Python/1411 빠진 카드.py
@@ -0,0 +1,15 @@
+﻿n = int(input())
+cards = []
+lost = []
+
+for i in range(1, n+1):
+    cards.append(i)
+
+for i in range(0, n-1):
+    num = int(input())
+    lost.append(num)
+
+for i in lost:
+    cards.remove(i)
+
+print(cards[0])
